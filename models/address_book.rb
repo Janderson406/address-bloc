8694607516e11edb01aeae59930fbe5a35f1e6a9
Jon Anderson <jondanderson@gmail.com>
2016-01-13 23:13:48 -0700

    Checkpoint 21 Address-Bloc CSV: Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,6 @@
 require_relative 'entry'
 #load library entry.rb relative to address_book.rb's file path
+require "csv"
 
 class AddressBook
  attr_accessor :entries
@@ -21,4 +22,17 @@ class AddressBook
    @entries.insert(index, Entry.new(name, phone_number, email))
    #insert a new entry into entries using the calculated `index
  end
+
+ def import_from_csv(file_name)
+   csv_text = File.read(file_name)  #reading the file, using File.read
+   csv = CSV.parse(csv_text, headers: true, skip_blanks: true) #use the CSV class to parse the file.
+   #The result of CSV.parse is an object of type CSV::Table (Class Method?)
+   csv.each do |row|
+     row_hash = row.to_hash #create a hash for each row
+     add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     #convert each row_hash to an Entry by using the add_entry method
+   end
+ end
+
+
 end
